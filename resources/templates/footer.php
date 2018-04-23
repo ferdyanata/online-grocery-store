48200da7b084f1af8dbbb2b94069de35315075e8
Ferdy <ferdy.anata@gmail.com>
2018-04-23 14:49:35 +1000

    Added jQuery library

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,3 +1,10 @@
 <script type="text/javascript" src="js/script.js"></script>
+
+<!-- CDN version 
+  <script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script> -->
+<script src="resources/library/jquery-3.3.1.slim.min.js"></script>
 </body>
 </html>
