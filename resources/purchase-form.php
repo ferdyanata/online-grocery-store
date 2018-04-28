ba2f07026d1e04bb62a29949f45a8ccf6fb0ac33
Ferdy <ferdy.anata@gmail.com>
2018-04-28 16:44:11 +1000

    Comment email test code
    - Comment on test code as it triggers automatically when a user visits the page.

--- resources/purchase-form.php
+++ resources/purchase-form.php
@@ -41,40 +41,42 @@
 </form>
 
 <?php
+    // @desc 
     // Test code for email
-    $to = "ferdy.anata@gmail.com";
-    $subject = "HTML email";
+
+    // $to = "ferdy.anata@gmail.com";
+    // $subject = "HTML email";
     
-    $message = "
-    <html>
-    <head>
-    <title>HTML email</title>
-    </head>
-    <body>
-    <p>This email contains HTML Tags!</p>
-    <table>
-    <tr>
-    <th>Firstname</th>
-    <th>Lastname</th>
-    </tr>
-    <tr>
-    <td>John</td>
-    <td>Doe</td>
-    </tr>
-    </table>
-    </body>
-    </html>
-    ";
+    // $message = "
+    // <html>
+    // <head>
+    // <title>HTML email</title>
+    // </head>
+    // <body>
+    // <p>This email contains HTML Tags!</p>
+    // <table>
+    // <tr>
+    // <th>Firstname</th>
+    // <th>Lastname</th>
+    // </tr>
+    // <tr>
+    // <td>John</td>
+    // <td>Doe</td>
+    // </tr>
+    // </table>
+    // </body>
+    // </html>
+    // ";
     
-    // Always set content-type when sending HTML email
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    // // Always set content-type when sending HTML email
+    // $headers = "MIME-Version: 1.0" . "\r\n";
+    // $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
     
-    // More headers
-    $headers .= 'From: <ferdy.anata@gmail.com>' . "\r\n";
-    // $headers .= 'Cc: myboss@example.com' . "\r\n";
+    // // More headers
+    // $headers .= 'From: <ferdy.anata@gmail.com>' . "\r\n";
+    // // $headers .= 'Cc: myboss@example.com' . "\r\n";
     
-    mail($to,$subject,$message,$headers);
+    // mail($to,$subject,$message,$headers);
 ?>
 
 <?php include_once 'includes/footer.php' ?>
