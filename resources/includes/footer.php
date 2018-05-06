8f05ed839b281d9ed0705c5250ca2f843e8fcff6
Ferdy <ferdy.anata@gmail.com>
2018-05-07 01:55:27 +1000

    Add semantic-ui for ui design

--- resources/includes/footer.php
+++ resources/includes/footer.php
@@ -1,4 +1,5 @@
 <script type="text/javascript" src="library/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="library/semantic-2.3/semantic.js"></script>
 <script type="text/javascript" src="../js/script.js"></script>
 </body>
 </html>
