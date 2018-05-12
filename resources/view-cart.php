fcc807019ca1a4f93151566406055e47e8166408
Unknown <zienjih@gmail.com>
2018-05-12 16:06:28 +1000

    Display unit quantity in detail page

--- resources/view-cart.php
+++ resources/view-cart.php
@@ -18,7 +18,8 @@ if(!empty($_GET["action"])) {
 			    $itemArray = array(
                     $product["product_id"]  =>  array(
                                                 'product_name'  =>  $product["product_name"],
-                                                'product_id'    =>  $product["product_id"], 
+                                                'product_id'    =>  $product["product_id"],
+                                                'unit_quantity' =>  $product["unit_quantity"],
                                                 'quantity'      =>  $_POST["quantity"], 
                                                 'unit_price'    =>  $product["unit_price"]));
 			
@@ -38,6 +39,7 @@ if(!empty($_GET["action"])) {
                         $_SESSION["cart_item"][$product["product_id"]] = array(
                                                 'product_name'  =>  $product["product_name"],
                                                 'product_id'    =>  $product["product_id"], 
+                                                'unit_quantity' =>  $product["unit_quantity"],
                                                 'quantity'      =>  $_POST["quantity"], 
                                                 'unit_price'    =>  $product["unit_price"]);
                     }
@@ -53,17 +55,31 @@ if(!empty($_GET["action"])) {
 }
 
 if(isset($_SESSION["cart_item"])) {
+    echo "<div id='head' class='item'>
+            <div class='description'> 
+                Products
+            </div> 
+            <div class='quantity'>
+                Quantity
+            </div>
+            <div class='quantity'>
+                Price
+            </div>
+          </div>";
     foreach ($_SESSION["cart_item"] as $item) {
-?>
-    <div id="view-cart-div">
-        <tr>
-            <td style="text-align:left;border-bottom:#F0F0F0 1px solid;"><strong><?php echo $item["product_name"]; ?></strong></td>
-            <td style="text-align:left;border-bottom:#F0F0F0 1px solid;"><?php echo $item["product_id"]; ?></td>
-            <td style="text-align:right;border-bottom:#F0F0F0 1px solid;"><?php echo $item["quantity"]; ?></td>
-            <td style="text-align:right;border-bottom:#F0F0F0 1px solid;"><?php echo "$".$item["unit_price"]; ?></td>
-        </tr>
-    </div>
-<?php
+        echo "<div class='item'>
+                <div class='description'> 
+                    <span>" . $item['product_name'] ."</span>
+                    <span>" . $item['unit_quantity'] ."</span>
+                    <span>$" . $item['unit_price'] . "</span>
+                </div> 
+                <div class='quantity'>"
+                    .$item['quantity'].   
+               "</div>
+                <div class='quantity'>$".
+                    $item['unit_price']*$item['quantity']."
+                </div>
+              </div>";
     }
 }
 
